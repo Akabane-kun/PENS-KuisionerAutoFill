ab9fa231493eddea3741fc16424859bc448bc7e0
Akabane-kun <ursawarrior157@gmail.com>
2021-09-08 17:13:20 +0700

    dhkgfsb Fixes #3

--- PraktikumAndTeori.js
+++ PraktikumAndTeori.js
@@ -4,7 +4,6 @@ javascript: (function () {
         var a = document.getElementById("a" + i + "_" + Math.floor(Math.random()*2+3));
         var b = document.getElementById("b" + i + "_" + Math.floor(Math.random()*2+3));
         var c = document.getElementById("c" + i + "_" + Math.floor(Math.random()*2+3));
-        var d = document.getElementById("d" + i + "_" + Math.floor(Math.random()*2+3));
         if (a != null){
             a.click();
         }
@@ -13,9 +12,6 @@ javascript: (function () {
         }
         if (c != null){
             c.click();
-        }
-        if (d != null){
-            d.click();
         }
     } 
 })();
